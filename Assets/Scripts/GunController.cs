c5ebca912c2eb1fa1a2875cd6a7c0325c8f3bf64
HKStudio011 <trucngonghinh2002@gmail.com>
2024-01-25 20:36:31 +0700

    rotaito gun

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
+    private Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        LookAtCursor();
+    }
+
+    void LookAtCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit))
+        {
+            target = hit.point;
+        }
+        this.transform.LookAt(target);
     }
 }
